ad6f0be7ac1027c0b98570e463d75e3eb2ff8d5b
CWRZ ZUHAYEEM <cwrzuhayeem@gmail.com>
2025-08-02 00:17:53 +0600

    Reformat .sol files with forge fmt

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -4,17 +4,12 @@ pragma solidity ^0.8.0;
 import {AggregatorV3Interface} from "@chainlink/contracts@1.3.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getPrice(
-        AggregatorV3Interface priceFeed
-    ) internal view returns (uint256) {
-        (, int256 answer, , , ) = priceFeed.latestRoundData();
+    function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
+        (, int256 answer,,,) = priceFeed.latestRoundData();
         return uint256(answer) * 1e10;
     }
 
-    function getConversionRate(
-        uint256 ethAmount,
-        AggregatorV3Interface priceFeed
-    ) internal view returns (uint256) {
+    function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
         uint256 ethPrice = getPrice(priceFeed);
         uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
         return ethAmountInUSD;
